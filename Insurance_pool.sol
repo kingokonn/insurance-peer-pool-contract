20d49974c4569477d4a96790636e198de17fc82a
kingokonn <kingsleyokon629@gmail.com>
2023-06-29 21:04:41 +0100

    update

--- Insurance_pool.sol
+++ Insurance_pool.sol
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-
 contract InsurancePeerPool {
     struct Member {
-        uint256 contribution;   // Contribution amount from member
-        uint256 insuranceAmount;  // Insurance amount allocated to the member
-        bool isActive;  // Whether the member is active or not
+        uint256 contribution; // Contribution amount from member
+        uint256 insuranceAmount; // Insurance amount allocated to the member
+        bool isActive; // Whether the member is active or not
     }
 
     mapping(address => Member) public members;
@@ -25,7 +24,10 @@ contract InsurancePeerPool {
     }
 
     modifier onlyOwner() {
-        require(msg.sender == owner, "Only the contract owner can call this function.");
+        require(
+            msg.sender == owner,
+            "Only the contract owner can call this function."
+        );
         _;
     }
 
@@ -45,21 +47,23 @@ contract InsurancePeerPool {
 
         members[msg.sender] = newMember;
         memberAddresses.push(msg.sender);
-
-        emit ContributionAdded(msg.sender, msg.value);
     }
 
     function contribute() external payable onlyActiveMember {
         require(msg.value > 0, "Contribution amount must be greater than 0.");
 
         members[msg.sender].contribution += msg.value;
-
         emit ContributionAdded(msg.sender, msg.value);
+
+        payable(address(this)).transfer(msg.value);
     }
 
     function claimInsurance(uint256 amount) external onlyActiveMember {
         require(amount > 0, "Claim amount must be greater than 0.");
-        require(amount <= members[msg.sender].insuranceAmount, "Insufficient insurance amount.");
+        require(
+            amount <= members[msg.sender].insuranceAmount,
+            "Insufficient insurance amount."
+        );
 
         members[msg.sender].insuranceAmount -= amount;
         payable(msg.sender).transfer(amount);
@@ -69,7 +73,10 @@ contract InsurancePeerPool {
 
     function withdrawContribution(uint256 amount) external onlyActiveMember {
         require(amount > 0, "Withdrawal amount must be greater than 0.");
-        require(amount <= members[msg.sender].contribution, "Insufficient contribution balance.");
+        require(
+            amount <= members[msg.sender].contribution,
+            "Insufficient contribution balance."
+        );
 
         members[msg.sender].contribution -= amount;
         payable(msg.sender).transfer(amount);
@@ -77,7 +84,10 @@ contract InsurancePeerPool {
         emit ContributionWithdrawn(msg.sender, amount);
     }
 
-    function allocateInsurance(address memberAddress, uint256 amount) external onlyOwner {
+    function allocateInsurance(
+        address memberAddress,
+        uint256 amount
+    ) external onlyOwner {
         require(members[memberAddress].isActive, "Member is inactive.");
         require(amount > 0, "Allocation amount must be greater than 0.");
 
@@ -106,9 +116,12 @@ contract InsurancePeerPool {
 
     function withdrawFunds(uint256 amount) external onlyOwner {
         require(amount > 0, "Withdrawal amount must be greater than 0.");
-        require(amount <= address(this).balance, "Insufficient contract balance.");
-            payable(msg.sender).transfer(amount);
+        require(
+            amount <= address(this).balance,
+            "Insufficient contract balance."
+        );
+        payable(msg.sender).transfer(amount);
 
-    emit FundsWithdrawn(msg.sender, amount);
-}
+        emit FundsWithdrawn(msg.sender, amount);
+    }
 }
